9112929c4eed7c8ac5416c73b03557da39816e0f
Aditya Sohal <adityasohal732@gmail.com>
2026-02-21 23:53:37 +0530

    made orders page

--- admin/src/pages/CustomersPage.jsx
+++ admin/src/pages/CustomersPage.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
-const CustomersPage = () => {
-  return (
-    <div>CustomersPage</div>
+function CustomerPage () {
+  return(
+    <div>CustomerPage</div>
   )
 }
 
-export default CustomersPage
+export default CustomerPage;
--- admin/src/pages/LoginPage.jsx
+++ admin/src/pages/LoginPage.jsx
@@ -7,19 +7,16 @@ function LoginPage() {
         <h1 className="text-5xl font-bold mb-6">
           Welcome to <span className="text-primary">Expo-Ecommerce</span>
         </h1>
-
         <p className="text-base-content/70 mb-10 text-lg">
           Please sign in to continue managing your store, track orders,
           monitor performance, and grow your business.
         </p>
-
         <div className="flex justify-center gap-4">
           <SignInButton mode="modal">
             <button className="btn btn-primary btn-lg">
               Sign In
             </button>
           </SignInButton>
-
           <SignUpButton mode="modal">
             <button className="btn btn-outline btn-primary btn-lg">
               Sign Up
