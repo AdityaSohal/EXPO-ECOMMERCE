a40c50a5120db3a1e5cf63738925e08df20abcca
Aditya Sohal <adityasohal732@gmail.com>
2026-02-22 00:04:22 +0530

    made customers page

--- admin/src/lib/api.js
+++ admin/src/lib/api.js
@@ -1,34 +1,49 @@
-import axiosInstance from "./axios.js";
+import axiosInstance from "./axios";
 
 export const productApi = {
-    getAll: async() => {
-        const {data} = await axiosInstance.get("/admin/products");
+    getAll: async () => {
+        const { data } = await axiosInstance.get("/admin/products");
         return data;
     },
-    create: async(formData) => {
-        const {data} = await axiosInstance.post("/admin/products",formData);
+
+    create: async (formData) => {
+        const { data } = await axiosInstance.post("/admin/products", formData);
         return data;
     },
-    update: async({id,formData}) => {
-        const {data} = await axiosInstance.post(`/admin/products/${id}`,formData);
+
+    update: async ({ id, formData }) => {
+        const { data } = await axiosInstance.put(`/admin/products/${id}`, formData);
+        return data;
+    },
+
+    delete: async (productId) => {
+        const { data } = await axiosInstance.delete(`/admin/products/${productId}`);
         return data;
     },
 };
 
 export const orderApi = {
-    getAll: async() => {
-        const {data} = await axiosInstance.get("/admin/orders");
+    getAll: async () => {
+        const { data } = await axiosInstance.get("/admin/orders");
         return data;
     },
-    updateStatus : async({orderId,staus}) => {
-        const {data} = await axiosInstance.patch(`/admin/orders/${orderId}/status`,{staus});
+
+    updateStatus: async ({ orderId, status }) => {
+        const { data } = await axiosInstance.patch(`/admin/orders/${orderId}/status`, { status });
         return data;
-    }
-}
+    },
+};
 
 export const statsApi = {
-    getDashboard: async() => {
-        const {data} = await axiosInstance.get("/admin/stats");
+    getDashboard: async () => {
+        const { data } = await axiosInstance.get("/admin/stats");
         return data;
-    }
-}
+    },
+};
+
+export const customerApi = {
+    getAll: async () => {
+        const { data } = await axiosInstance.get("/admin/customers");
+        return data;
+    },
+};
